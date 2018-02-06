264584e6c9a386cd77faa53759aa81bc60c8ad79
Rodolfo Cárcamo Mesa <carcamomesa@gmail.com>
2018-02-05 23:04:17 -0500

    Nuevos archivos de documentación.

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "environment",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-02-06T03:51:51.161Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
